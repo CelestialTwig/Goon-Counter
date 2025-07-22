e87ea0d267055233e4ec9fb341c8e79f8636ec1e
CelestialTwig <198583192+CelestialTwig@users.noreply.github.com>
2025-07-23 02:06:02 +0300

    Added a badge system

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const incrementButton = document.querySelector(".gButton");
     const countLabel = document.querySelector(".actualCounter");
-    
+    const badgesSideBar = document.querySelector(".sidebar ul");
+    const customAlert = document.querySelector('.custom-alert');
+    const confirmBtn = document.getElementById('confirmation')
 
 
     incrementButton.addEventListener("click", () => {
         let score = parseInt(localStorage.getItem('score'))
         let newScore = score + 1;
         localStorage.setItem('score', newScore);
-        countLabel.textContent = parseInt(localStorage.getItem('score'));
+        countLabel.textContent = parseInt(localStorage.getItem('score'))
+         if (parseInt(localStorage.getItem('score')) == 100) {
+            let newBadge = document.createElement('li');
+            newBadge.classList.add("master-baiter-badge")
+            newBadge.textContent = "MASTER BAITER";
+            badgesSideBar.appendChild(newBadge);
+            let badgeLi = document.querySelector(".master-baiter-badge");
+            let img = document.createElement("img");
+            img.src = "Master-Baiter-Badge.webp"
+            img.alt = "Master-Baiter-Badge";
+            img.classList.add("badge-icon");
+            badgeLi.appendChild(img);
+            customAlert.style.display = "flex";
+        }
+
     });
 
     if (!localStorage.getItem('score')){
@@ -17,7 +33,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     countLabel.textContent = localStorage.getItem('score');
 
-
+   confirmBtn.addEventListener('click', function(){
+    customAlert.style.display = 'none';
+   })
 
 
 
